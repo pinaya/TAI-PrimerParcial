ec5dd7c22aaee4f659b049ac4c19f2a266867c52
pinaya <82481701+pinaya@users.noreply.github.com>
2024-04-18 17:45:08 -0400

    Add files via upload

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const usuarioEsquema = new mongoose.Schema({
-    nombre : String,
-    contrasena : String
+    nombreusuario: {type: String, required:true, unique: true},
+    correo: {type: String, required: true, unique: true},
+    contrasena: {type: String, required: true},
+    
+    tokenSesion:{type:String} //Almacena el token de sesion
+});
+
+//Middleware para hashear la contrasena
+usuarioEsquema.pre('save', async function (next){
+    if(this.isModified('contrasena')){
+        this.contrasena = await bcrypt.hash(this.contrasena, 10);
+    }
+    next();
 })
+
+//Comparar contrasenas
+usuarioEsquema.methods.comparePassword = async function (contrasenaEsperada){
+    return await bcrypt.compare(contrasenaEsperada, this.contrasena);
+}
 
 const UsuarioModel = mongoose.model('Usuario',usuarioEsquema,'usuario');
 module.exports = UsuarioModel;
